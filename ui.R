49b51f497cf4e688d445e1544a7a2ec059083018
ajayceindia <ajayceindia@yahoo.com>
2020-11-22 18:31:55 +0530

    Update

---
+++ ui.R
@@ -0,0 +1,32 @@
+suppressWarnings(library(shiny))
+suppressWarnings(library(markdown))
+shinyUI(navbarPage("Capstone Project",
+                   tabPanel("Predict the Next Word",
+                            HTML("<strong>Date: 22/11/2020</strong>"),
+                            br(),
+                            # Sidebar
+                            sidebarLayout(
+                                sidebarPanel(
+                                    helpText("Enter a phrase/word/sentence to begin the next word prediction"),
+                                    textInput("inputString", "Enter a partial sentence here",value = ""),
+                                    br(),
+                                    br(),
+                                    br(),
+                                    br()
+                                ),
+                                mainPanel(
+                                    h2("Predicted Next Word"),
+                                    verbatimTextOutput("prediction"),
+                                    strong("Sentence Input:"),
+                                    tags$style(type='text/css', '#text1 {background-color: rgba(255,255,0,0.40); color: blue;}'), 
+                                    textOutput('text1'),
+                                    br(),
+                                    strong("Note:"),
+                                    tags$style(type='text/css', '#text2 {background-color: rgba(255,255,0,0.40); color: black;}'),
+                                    textOutput('text2')
+                                )
+                            )
+                            
+                   )
+)
+)
